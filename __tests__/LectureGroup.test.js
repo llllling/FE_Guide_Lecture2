248ae78e3b77decef876c389ad0b5737c4212a40
MISEONGKIM <rlaaltjd46@naver.com>
2023-09-25 13:52:20 +0900

    LectureGroup 테스트 수정

--- __tests__/LectureGroup.test.js
+++ __tests__/LectureGroup.test.js
@@ -5,7 +5,9 @@ import "@testing-library/jest-dom";
 describe("강의 그룹은", () => {
   it("제목을 가지고 있어야 한다.", () => {
     const { getByRole } = render(<LectureGroup />);
-    const lectureTitle = getByRole("heading");
+    const lectureTitle = getByRole("heading", {
+      name: "이번주 베스트셀링 강의"
+    });
 
     expect(lectureTitle).toBeInTheDocument();
   });
