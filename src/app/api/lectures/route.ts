e7d1960453161efcb458fa5811ec8f7bddb9f41e
MISEONGKIM <rlaaltjd46@naver.com>
2023-09-25 14:02:14 +0900

    leactures api 추가

--- src/app/api/lectures/route.ts
+++ src/app/api/lectures/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 
 const mockCategory = {
   id: 0,
-  name: "프로그래밍",
+  name: "프로그래밍"
 };
 
 const mockTags = [
   { id: 0, title: "평생소장" },
   { id: 1, title: "AWS" },
-  { id: 2, title: "DevOps" },
+  { id: 2, title: "DevOps" }
 ];
 const leacturesData = {
   lectureList: [
@@ -21,9 +21,9 @@ const leacturesData = {
         "Stable diffusion부터 포토샵까지! 반복 업무 지옥에서 해방시켜줄 AI 활용법",
       thumb: "/thumb.jpg",
       isHot: true,
-      isNew: true,
-    },
-  ],
+      isNew: true
+    }
+  ]
 };
 
 export async function GET(request: Request) {
